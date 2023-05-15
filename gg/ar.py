bbf69318132c8cf3e46ac0ea0cc7bc254b632de8
Arjun vardhan <arjun.vardhan.862@k12.friscoisd.org>
2023-05-15 17:19:09 -0500

    first time checkin

---
+++ gg/ar.py
@@ -0,0 +1,52 @@
+def add(number1, number2): 
+    return (number1 + number2) 
+  
+#Function to perform subtraction of two numbers
+def subtract(number1, number2): 
+    return (number1 - number2)
+  
+#Function to perform multiplication of two numbers 
+def multiply(number1, number2): 
+    return (number1 * number2) 
+  
+#Function to perform division of two numbers 
+def divide(number1, number2): 
+    return (number1 / number2)
+
+def exponent(number1, number2): 
+    return (number1 ** number2)
+  
+print("Operation Menu -\n"
+        "1. Addition of two numbers\n" 
+        "2. Subtraction of two numbers\n" 
+        "3. Multiplication of two numbers\n" 
+        "4. Division of two numbers\n"
+        "5. indicating an exponent kys\n") 
+  
+  
+#Taking input from the user  
+select = input("Select operations form 1, 2, 3, 4, 5:") 
+  
+num1 = int(input("Enter first number: ")) 
+num2 = int(input("Enter second number: ")) 
+  
+if select == '1': 
+    print(num1, "+", num2, "=", 
+                    add(num1, num2)) 
+  
+elif select == '2': 
+    print(num1, "-", num2, "=", 
+                    subtract(num1, num2)) 
+  
+elif select == '3': 
+    print(num1, "*", num2, "=", 
+                    multiply(num1, num2)) 
+  
+elif select == '4': 
+    print(num1, "/", num2, "=", 
+                    divide(num1, num2)) 
+elif select == '5': 
+    print(num1, "**", num2, "=", 
+                    exponent(num1, num2))
+else: 
+    print("Invalid input!")
